be2981896babec839d9251fd28f6e31abbe00c21
vitorvargas052-cmd <vitorvargas052@gmail.com>
2025-11-03 23:18:55 -0300

    Update supabaseclient.js

--- utils/supabaseclient.js
+++ utils/supabaseclient.js
@@ -1,8 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-// 🔗 Conexão única com o Supabase (usada em todo o projeto)
+// 🔗 Conexão única com o Supabase
 const supabaseUrl = "https://zqkksqfjwjznbkmivycc.supabase.com";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpxa2tzcWZqd2p6bmJrbWl2eWNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2NjU5NjMsImV4cCI6MjA3NzI0MTk2M30.wiAKtsG4-or0CwW1kjRBx3ljFLnRLKhR6TXD202BcF0";
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  realtime: { params: { eventsPerSecond: 10 } },
+});
